c4d40d4b3f8beef7f8e0f76bd2a9a95997329c5f
Bence Papp <113164265+pbence11@users.noreply.github.com>
2024-09-30 09:10:58 +0200

    logo was added

---
+++ todo/src/App.mjs
@@ -0,0 +1,13 @@
+import './styles/App.css';
+import ToDo from './components/ToDoTable.js';
+function App() {
+  return (
+    <div className='main-container'>
+      <ToDo></ToDo>
+    </div>
+  );
+}
+
+export default App;
+
+
